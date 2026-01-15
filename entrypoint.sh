be694e955773f1aae6e9a1dae724fbc60bec781a
kuisa <32757027+kuisa@users.noreply.github.com>
2026-01-15 22:18:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,10 @@ usermod -aG sudo $SSH_USER
 echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
 echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
 
-echo "Starting cloudflared with token"
+echo "Service Starting..."
 nohup /usr/bin/cloudflared --no-autoupdate tunnel run --token $CLOUDFLARED_TOKEN > /dev/null 2>&1 &
-
-echo "Starting ttyd"
 nohup /ssh/ttyd -6 -p 7681 -c kof97zip:kof97boss -W bash 1>/dev/null 2>&1 &
+echo "Service Started."
 
 if [ -n "$START_CMD" ]; then
     set -- $START_CMD
