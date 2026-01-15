42ff7b800a8e1a8e8b3fd0afbeeca58ddb2aec82
kuisa <32757027+kuisa@users.noreply.github.com>
2026-01-16 00:59:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ echo "Service Starting..."
 nohup /usr/bin/cloudflared --no-autoupdate tunnel run --token $CLOUDFLARED_TOKEN > /dev/null 2>&1 &
 nohup /ssh/ttyd -6 -p 7681 -c kof97zip:kof97boss -W bash 1>/dev/null 2>&1 &
 nohup /usr/local/x-ui/x-ui > /dev/null 2>&1 &
+nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /usr/local/x-ui/bin/config.json > /dev/null 2>&1 &
 echo "Service Started."
 
 if [ -n "$START_CMD" ]; then
