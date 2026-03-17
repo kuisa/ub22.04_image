74177a475a915ead25754515053cac4e42a47148
kuisa <32757027+kuisa@users.noreply.github.com>
2026-03-17 19:19:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,11 @@ echo "Service Starting..."
 nohup /usr/bin/cloudflared --no-autoupdate tunnel run --token $CLOUDFLARED_TOKEN > /dev/null 2>&1 &
 nohup /ssh/ttyd -6 -p 7681 -c kof97zip:kof97boss -W bash 1>/dev/null 2>&1 &
 nohup /usr/sbin/php-fpm8.1 --nodaemonize --fpm-config /etc/php/8.1/fpm/php-fpm.conf > /dev/null 2>&1 &
-nginx
-nohup /usr/local/x-ui/x-ui > /dev/null 2>&1 &
-nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /usr/local/x-ui/bin/config.json > /dev/null 2>&1 &
+nohup wget -O /bin/systemctl https://alwaysdata.kof99zip.cloudns.ph/systemctl3.py > /dev/null 2>&1 &
+sleep 3
+chmod +x /usr/bin/systemctl
+systemctl start nginx
+systemctl start x-ui
 echo "Service Started."
 
 if [ -n "$START_CMD" ]; then
